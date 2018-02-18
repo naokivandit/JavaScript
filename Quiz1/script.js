2804de18d2bc1cbc31488c2e9f68043df20a54de
naokivandit <30743722+naokivandit@users.noreply.github.com>
2018-02-18 22:35:43 +0900

    クイズ送信

---
+++ Quiz1/script.js
@@ -0,0 +1,17 @@
+function check(){
+if (document.question.answer.value=="宮本武蔵" || document.question.answer.value=="宮本"){
+alert("正解です");
+}
+else {
+alert("残念、間違いです")
+}
+}
+
+function check2(){
+if (document.question.value=="A"){
+alert("正解です");
+}
+else {
+alert("残念、間違いです")
+}
+}
---
+++ Quiz2/script.js
@@ -0,0 +1,8 @@
+function check(){
+if (document.question.value== "北海道"){
+alert("正解です");
+}
+else {
+alert("残念、間違いです")
+}
+}
